76b4ea962f083c7abd2108e60d4314da81253b21
dev81log <dev81log@gmail.com>
2022-05-29 12:28:02 -0300

    Iniciando estudos de litas em c#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var myArray = new int[7] { 3, 55, 60, 36, 10, 27, 16 };
+            myArray[2] = 22;
+            myArray[3] = 23;
+
+            Console.WriteLine(myArray[2]);
+            Console.WriteLine(myArray[3]);
+            
+            Console.WriteLine("===========");
+            
+            for (var index = 0; index < 7; index++)
+            {
+                Console.WriteLine(myArray[index]);
+            }
+        }
+    }
+}
