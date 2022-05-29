a87e99511cfb341b927535199b1c7792b1d5f936
dev81log <dev81log@gmail.com>
2022-05-29 13:08:03 -0300

    Finalizando estudos basicos de Arrays em c#

--- Program.cs
+++ Program.cs
@@ -1,24 +1,44 @@
-﻿namespace Arrays
+﻿namespace Arrays;
+
+internal static class Program
 {
-    internal static class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.Clear();
+        Console.Clear();
 
-            var myArray = new int[7] { 3, 55, 60, 36, 10, 27, 16 };
-            myArray[2] = 22;
-            myArray[3] = 23;
+        var myArray = new int[7] { 3, 55, 60, 36, 10, 27, 16 };
+        myArray[2] = 22;
+        myArray[3] = 23;
 
-            Console.WriteLine(myArray[2]);
-            Console.WriteLine(myArray[3]);
-            
-            Console.WriteLine("===========");
-            
-            for (var index = 0; index < 7; index++)
-            {
-                Console.WriteLine(myArray[index]);
-            }
-        }
+        Console.WriteLine(myArray[2]);
+        Console.WriteLine(myArray[3]);
+
+        Console.WriteLine("===========");
+
+        for (var index = 0; index < 7; index++) // manual index assignment of the array
+            Console.WriteLine(myArray[index]);
+
+
+        Console.WriteLine("===========");
+
+        foreach (var index in myArray) // automatic index assignment of the array
+            Console.WriteLine(index);
+
+
+        Console.WriteLine("===========");
+
+
+        var offices = new Worker[2]; // length of the array
+        offices[0] = new Worker() { NumberSecurity = 2244, Name = "Andrei" }; // constructor position of the array 
+        offices[1] = new Worker() { NumberSecurity = 3355, Name = "Julius" };
+
+        foreach (var office in offices)
+            Console.WriteLine($"Number security: {office.NumberSecurity} Name: {office.Name}");
+    }
+
+    private struct Worker // struct representing Worker objects
+    {
+        public int NumberSecurity { get; set; }
+        public string Name { get; set; }
     }
 }
